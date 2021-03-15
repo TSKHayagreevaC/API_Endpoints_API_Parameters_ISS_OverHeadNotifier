d96c32070bb76ad35204fe5eff1bbe1391703645
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-03-15 15:36:42 +0530

    commit with quotation display app.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # import requests
-# 
+#
 # response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# 
+#
 # response.raise_for_status()
-# 
+#
 # data = response.json()
-# 
+#
 # longitude = data["iss_position"]["longitude"]
 # latitude = data["iss_position"]["latitude"]
 # iss_position = (longitude, latitude)
-# 
+#
 # print(iss_position)
 
 # if response.status_code != 404:
@@ -21,4 +21,30 @@
 
 import requests
 
-API_KANYE = "https://api.kanye.rest"
+from tkinter import *
+import requests
+
+
+def get_quote():
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+
+
+window = Tk()
+window.title("Kanye Quote Is ...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+window.mainloop()
